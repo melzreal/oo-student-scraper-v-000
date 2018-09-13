3fa871e983e556dba705b34276a92d55d5b23820
Melissa Real <melissa.real@hotmail.com>
2018-09-13 16:28:30 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,15 +6,15 @@ class Scraper
   def self.scrape_index_page(index_url)
     kicks =  Nokogiri::HTML(open(index_url))
 
-    students = []
+    # students = []
     kicks.css(".student-card").collect { |student|
-      students << {
+      {
         :name => student.css(".student-name").text,
         :location => student.css(".student-location").text,
         :profile_url => student.css("a").attr("href").value
       }
      }
-     students
+     # students
   end
 
   def self.scrape_profile_page(profile_url)
